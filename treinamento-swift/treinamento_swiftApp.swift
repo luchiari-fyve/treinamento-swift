1f419f74079103eda9aaa3e701866c48e34675e4
Laura Luchiari <laura.luchiari@fyve.com.br>
2023-09-12 14:04:00 -0300

    Initial Commit

---
+++ treinamento-swift/treinamento_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  treinamento_swiftApp.swift
+//  treinamento-swift
+//
+//  Created by Laura Ranucci Luchiari on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct treinamento_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
